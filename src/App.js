d4ddc66c5958159ce4809115b81323d2ad240fa9
Kwabs-97 <khobewayne@gmail.com>
2023-10-25 23:16:16 +0000

    fixed navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-   
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,42 @@
-import React from 'react'
-import styles from '../styles/Navbar.module.css'
+/** @format */
+
+import React from "react";
+import styles from "../styles/Navbar.module.css";
+import { AiOutlineMenu } from "react-icons/ai";
+import { BsArrowUpLeft } from "react-icons/bs";
 
 function Navbar() {
   return (
-    <nav></nav>
-  )
+    <nav>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <div className={styles.logobg}>
+            <BsArrowUpLeft color="black" size={15} />
+          </div>
+          <span>Raft</span>
+        </div>
+        <div className={styles.pcNav}>
+          <ul>
+            <li>
+              <a href="#">Solutions</a>
+            </li>
+            <li>
+              <a href="#">Learn</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.login}>
+          <a href="#">Login</a>
+          <button>Get started</button>
+        </div>
+
+        <div className={styles.hero}>{<AiOutlineMenu color="white" size={15} />}</div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;

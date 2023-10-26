8e4082850edac6da5838917046300d044207b1d8
Kwabs-97 <khobewayne@gmail.com>
2023-10-26 14:41:48 +0000

    carousel done

--- src/components/Features/Carousel.js
+++ src/components/Features/Carousel.js
@@ -2,14 +2,16 @@
 
 import React from "react";
 import images from "../../utils/images";
-import styles from '../../styles/Carousel.module.css'
+import styles from "../../styles/Carousel.module.css";
 
 function Carousel() {
   return (
     <div className={styles.carouselContainer}>
-      {images.map((image) => {
-        return <img src={image} key={image} />;
-      })}
+      <div className={styles.scrollWrapper}>
+        {images.map((image) => {
+          return <img src={image} key={image} />;
+        })}
+      </div>
     </div>
   );
 }

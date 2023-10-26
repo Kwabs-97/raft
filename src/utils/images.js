151a81bd6b393164dab969eb1ecf504103975878
Kwabs-97 <khobewayne@gmail.com>
2023-10-26 13:52:00 +0000

    added carousel component and passed images into util

---
+++ src/utils/images.js
@@ -0,0 +1,8 @@
+/** @format */
+
+import cnn from "../assets/cnn-logo 1.png";
+import forbes from "../assets/Forbes_logo 1.png";
+import bloomberg from "../assets/bloomberg-1.png";
+import cnbc from "../assets/cnbc-1.png";
+import reteurs from "../assets/reuters-2-1 1.png";
+export default images = [cnbc, bloomberg, forbes, reteurs, cnn];

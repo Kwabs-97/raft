72b1756a2fd73c9e40c832638d059b4dfc5a0c53
Kwabs-97 <khobewayne@gmail.com>
2023-10-26 21:11:40 +0000

    hero section 3 retouched

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,12 +3,14 @@
 import React from "react";
 import HeroSection1 from "../components/HeroSection1";
 import HeroSection2 from "../components/HeroSection2";
+import HeroSection3 from "../components/HeroSection3";
 
 function Main() {
   return (
     <main>
       <HeroSection1 />
       <HeroSection2 />
+      <HeroSection3 />
     </main>
   );
 }

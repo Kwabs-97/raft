47b60f06515221dc674b460fac108d3521a15f57
Kwabs-97 <khobewayne@gmail.com>
2023-10-26 11:51:45 +0000

    restructuring for hero sections

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@
 import "./App.css";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
+import Main from "./pages/Main";
 
 function App() {
   return (
     <>
       <Navbar />
       <Header />
+      <Main />
     </>
   );
 }

0159d912fbabb20d5360c60802cc1a97bf23dd68
Kwabs-97 <khobewayne@gmail.com>
2023-10-26 15:13:15 +0000

    start of heroSection 3

---
+++ src/components/HeroSection3.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../styles/HeroSection3.module.css'
+
+function HeroSection3() {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  )
+}
+
+export default HeroSection3

64c5156f32b7af6a8a1b10a8de6b0828a86b6698
Kwabs-97 <khobewayne@gmail.com>
2023-10-26 23:21:58 +0000

    hero section containers

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -4,6 +4,7 @@ import React from "react";
 import HeroSection1 from "../components/HeroSection1";
 import HeroSection2 from "../components/HeroSection2";
 import HeroSection3 from "../components/HeroSection3";
+import HeroSection4 from "../components/HeroSection4";
 
 function Main() {
   return (
@@ -11,6 +12,7 @@ function Main() {
       <HeroSection1 />
       <HeroSection2 />
       <HeroSection3 />
+      <HeroSection4 />
     </main>
   );
 }

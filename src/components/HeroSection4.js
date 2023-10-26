0c36f57a8904cf2fd2bcd9c5a08f42c52c2c73ac
Kwabs-97 <khobewayne@gmail.com>
2023-10-26 23:22:06 +0000

    hero section containers

---
+++ src/components/HeroSection4.js
@@ -0,0 +1,47 @@
+/** @format */
+
+import React from "react";
+import Container from "../components/UI/Container";
+import styles from "../styles/HeroSection4.module.css";
+function HeroSection4() {
+  return (
+    <>
+      <Container>
+        <div className={styles.containerOne}>
+          <div className={styles.vector}>
+            <img
+              loading="lazy"
+              srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/6175e8d6-550c-417a-9569-255288d000de?apiKey=0179e5cdf4434003ace2ef50a4f0813c&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/6175e8d6-550c-417a-9569-255288d000de?apiKey=0179e5cdf4434003ace2ef50a4f0813c&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/6175e8d6-550c-417a-9569-255288d000de?apiKey=0179e5cdf4434003ace2ef50a4f0813c&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/6175e8d6-550c-417a-9569-255288d000de?apiKey=0179e5cdf4434003ace2ef50a4f0813c&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/6175e8d6-550c-417a-9569-255288d000de?apiKey=0179e5cdf4434003ace2ef50a4f0813c&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/6175e8d6-550c-417a-9569-255288d000de?apiKey=0179e5cdf4434003ace2ef50a4f0813c&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/6175e8d6-550c-417a-9569-255288d000de?apiKey=0179e5cdf4434003ace2ef50a4f0813c&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/6175e8d6-550c-417a-9569-255288d000de?apiKey=0179e5cdf4434003ace2ef50a4f0813c&"
+              alt="Description of the image"
+            />
+          </div>
+
+          <div className={styles.main}>
+            <h4>Seamless Payments</h4>
+            <p>Enjoy secure, seamless transactions that make managing your money a breeze.</p>
+          </div>
+        </div>
+      </Container>
+
+      <Container>
+        <div className={styles.containerTwo}>
+          <div className={styles.vector}>
+            <img
+              loading="lazy"
+              srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/e9061b40-b63d-4d81-b262-69e1d9728c2d?apiKey=0179e5cdf4434003ace2ef50a4f0813c&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/e9061b40-b63d-4d81-b262-69e1d9728c2d?apiKey=0179e5cdf4434003ace2ef50a4f0813c&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/e9061b40-b63d-4d81-b262-69e1d9728c2d?apiKey=0179e5cdf4434003ace2ef50a4f0813c&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/e9061b40-b63d-4d81-b262-69e1d9728c2d?apiKey=0179e5cdf4434003ace2ef50a4f0813c&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/e9061b40-b63d-4d81-b262-69e1d9728c2d?apiKey=0179e5cdf4434003ace2ef50a4f0813c&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/e9061b40-b63d-4d81-b262-69e1d9728c2d?apiKey=0179e5cdf4434003ace2ef50a4f0813c&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/e9061b40-b63d-4d81-b262-69e1d9728c2d?apiKey=0179e5cdf4434003ace2ef50a4f0813c&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/e9061b40-b63d-4d81-b262-69e1d9728c2d?apiKey=0179e5cdf4434003ace2ef50a4f0813c&"
+              className="aspect-[1.79] object-cover object-center w-full overflow-hidden"
+              alt="Description of the image"
+            />
+          </div>
+
+          <div className={styles.main}>
+            <h4>Seamless Payments</h4>
+            <p>Enjoy secure, seamless transactions that make managing your money a breeze.</p>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+}
+
+export default HeroSection4;
---
+++ src/components/UI/Container.js
@@ -0,0 +1,10 @@
+/** @format */
+
+import React from "react";
+import styles from "../../styles/Container.module.css";
+
+function Container({ children }) {
+  return <div className={styles.container}>{children}</div>;
+}
+
+export default Container;

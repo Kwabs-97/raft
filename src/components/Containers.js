65193c9d99183a8da8fb5da05fed4867096bbe32
Kwabs-97 <khobewayne@gmail.com>
2023-10-27 07:36:36 +0000

    vector background

--- src/components/Containers.js
+++ src/components/Containers.js
@@ -29,14 +29,16 @@ function Containers() {
 
         <Container>
           <div className={styles.containerTwo}>
-            <div className={styles.vector}>
-              <img
-                loading="lazy"
-                srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/e9061b40-b63d-4d81-b262-69e1d9728c2d?apiKey=0179e5cdf4434003ace2ef50a4f0813c&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/e9061b40-b63d-4d81-b262-69e1d9728c2d?apiKey=0179e5cdf4434003ace2ef50a4f0813c&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/e9061b40-b63d-4d81-b262-69e1d9728c2d?apiKey=0179e5cdf4434003ace2ef50a4f0813c&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/e9061b40-b63d-4d81-b262-69e1d9728c2d?apiKey=0179e5cdf4434003ace2ef50a4f0813c&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/e9061b40-b63d-4d81-b262-69e1d9728c2d?apiKey=0179e5cdf4434003ace2ef50a4f0813c&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/e9061b40-b63d-4d81-b262-69e1d9728c2d?apiKey=0179e5cdf4434003ace2ef50a4f0813c&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/e9061b40-b63d-4d81-b262-69e1d9728c2d?apiKey=0179e5cdf4434003ace2ef50a4f0813c&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/e9061b40-b63d-4d81-b262-69e1d9728c2d?apiKey=0179e5cdf4434003ace2ef50a4f0813c&"
-                className="aspect-[1.79] object-cover object-center w-full overflow-hidden"
-                alt="Description of the image"
-              />
-            </div>
+              <div className={styles.vector}>
+            <VectorContainers>
+                <img
+                  loading="lazy"
+                  srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/e9061b40-b63d-4d81-b262-69e1d9728c2d?apiKey=0179e5cdf4434003ace2ef50a4f0813c&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/e9061b40-b63d-4d81-b262-69e1d9728c2d?apiKey=0179e5cdf4434003ace2ef50a4f0813c&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/e9061b40-b63d-4d81-b262-69e1d9728c2d?apiKey=0179e5cdf4434003ace2ef50a4f0813c&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/e9061b40-b63d-4d81-b262-69e1d9728c2d?apiKey=0179e5cdf4434003ace2ef50a4f0813c&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/e9061b40-b63d-4d81-b262-69e1d9728c2d?apiKey=0179e5cdf4434003ace2ef50a4f0813c&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/e9061b40-b63d-4d81-b262-69e1d9728c2d?apiKey=0179e5cdf4434003ace2ef50a4f0813c&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/e9061b40-b63d-4d81-b262-69e1d9728c2d?apiKey=0179e5cdf4434003ace2ef50a4f0813c&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/e9061b40-b63d-4d81-b262-69e1d9728c2d?apiKey=0179e5cdf4434003ace2ef50a4f0813c&"
+                  className="aspect-[1.79] object-cover object-center w-full overflow-hidden"
+                  alt="Description of the image"
+                />
+            </VectorContainers>
+              </div>
 
             <div className={styles.main}>
               <h4>Seamless Payments</h4>
--- src/components/UI/VectorContainers.js
+++ src/components/UI/VectorContainers.js
@@ -2,12 +2,15 @@
 
 import React from "react";
 import styles from "../../styles/VectorContainers.module.css";
+import vector from "../../assets/Vector/Vector.svg";
+import { ReactSVG } from "react-svg";
 
 function VectorContainers({ children }) {
   return (
     <div className={styles.vectorContainer}>
       {children}
       <div className={styles.overlay}></div>
+      <ReactSVG src={vector} />
     </div>
   );
 }

d5d29f7c46bedc7b458c8667b5ed8bf1c49bb622
Kwabs-97 <khobewayne@gmail.com>
2023-10-27 20:13:01 +0000

    financial freedom component added

---
+++ src/components/FinancialFreedom.js
@@ -0,0 +1,21 @@
+/** @format */
+
+import React from "react";
+import styles from '../styles/FinancialFreedom.module.css'
+
+function FinancialFreedom() {
+  return (
+    <div className={styles.container} >
+          <header>
+              <h4>
+                  
+              </h4>
+      </header>
+      <main></main>
+      <footer></footer>
+      <aside></aside>
+    </div>
+  );
+}
+
+export default FinancialFreedom;

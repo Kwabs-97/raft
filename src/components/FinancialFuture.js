1bcb060f21bd4571b3b41a16b8aaca565571889e
Kwabs-97 <khobewayne@gmail.com>
2023-10-29 14:35:25 +0000

    numbers done

--- src/components/FinancialFuture.js
+++ src/components/FinancialFuture.js
@@ -4,9 +4,9 @@ import React from "react";
 import { ReactSVG } from "react-svg";
 
 import styles from "../styles/FinancialFuture.module.css";
-import card1 from '../assets/Financial Future/Frame 35.svg'
-import card2 from '../assets/Financial Future/Frame 1000001583.svg'
-import img from '../assets/Financial Future/Frame 150.svg'
+import card1 from "../assets/Financial Future/Frame 35.svg";
+import card2 from "../assets/Financial Future/Frame 1000001583.svg";
+import img from "../assets/Financial Future/Frame 150.svg";
 function FinancialFuture() {
   return (
     <div className={styles.container}>
@@ -17,11 +17,28 @@ function FinancialFuture() {
           simplifies saving and investing, making it easier than ever.
         </p>
       </div>
-          <div className={styles.cards}>
-              <ReactSVG src={card1} />
-              <ReactSVG src={card2} />
+      <div className={styles.cards}>
+        <ReactSVG src={card1} />
+        <ReactSVG src={card2} />
       </div>
-      <div className={styles.numbers}></div>
+      <div className={styles.numbers}>
+        <section>
+          <h4>50+</h4>
+          <p>CITIES</p>
+        </section>
+        <section>
+          <h4>50,100+</h4>
+          <p>TRANSACTIONS</p>
+        </section>
+        <section>
+          <h4>3M+</h4>
+          <p>USERS</p>
+        </section>
+        <section>
+          <h4>5</h4>
+          <p>USER RATINGS</p>
+        </section>
+      </div>
       <div className={styles.image}></div>
     </div>
   );

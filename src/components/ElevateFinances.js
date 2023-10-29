56fd8b8f3e4050faf3aa53015f7495af1413c7ac
Kwabs-97 <khobewayne@gmail.com>
2023-10-29 13:01:03 +0000

    elevate finances done

--- src/components/ElevateFinances.js
+++ src/components/ElevateFinances.js
@@ -1,10 +1,18 @@
 /** @format */
 
 import styles from "../styles/ElevateFinances.module.css";
+
+//svgs for mobile design
 import seamlessSVG from "../assets/ElevateFinances SVGs/Frame 88.svg";
 import smartInvestingSVG from "../assets/ElevateFinances SVGs/Frame 1000001576.svg";
 import wealth from "../assets/ElevateFinances SVGs/Frame 1000001577.svg";
 import financialPlanning from "../assets/ElevateFinances SVGs/Frame 1000001578.svg";
+
+//svgs for laptop design
+import seamlessSVGLg from "../assets/ElevateFinances SVGs/Frame 88-lg.svg";
+import smartInvestingSVGLg from "../assets/ElevateFinances SVGs/Frame 32.svg";
+import wealthLg from "../assets/ElevateFinances SVGs/Frame 36.svg";
+import financialPlanningLg from "../assets/ElevateFinances SVGs/Frame 34.svg";
 
 import { ReactSVG } from "react-svg";
 function ElevateFinances() {
@@ -17,7 +25,9 @@ function ElevateFinances() {
           you covered. Join us and unlock your financial potential today.
         </p>
       </div>
-      <div className={styles.container}>
+
+      {/* container for mobile design*/}
+      <div className={styles.containerMobile}>
         <section className={styles.section1}>
           <ReactSVG src={seamlessSVG} className={styles.seamless} />
           <ReactSVG src={smartInvestingSVG} className={styles.smartInvestingSVG} />
@@ -25,6 +35,18 @@ function ElevateFinances() {
         <section className={styles.section2}>
           <ReactSVG src={wealth} className={styles.wealth} />
           <ReactSVG src={financialPlanning} className={styles.financialPlanning} />
+        </section>
+      </div>
+
+      {/* container for desktop design */}
+      <div className={styles.containerDesktop}>
+        <section className={styles.section1}>
+          <ReactSVG src={seamlessSVGLg} />
+          <ReactSVG src={smartInvestingSVGLg} />
+        </section>
+        <section className={styles.section2}>
+          <ReactSVG src={wealthLg} />
+          <ReactSVG src={financialPlanningLg} />
         </section>
       </div>
     </div>

1d2b25be4d4e2660a79f1713ca82fa6b80c10a17
Kwabs-97 <khobewayne@gmail.com>
2023-10-29 13:54:00 +0000

    financial freedom footer done

--- src/components/FinancialFreedom.js
+++ src/components/FinancialFreedom.js
@@ -1,8 +1,14 @@
 /** @format */
 
 import React from "react";
-import styles from "../styles/FinancialFreedom.module.css";
+import { ReactSVG } from "react-svg";
 
+import styles from "../styles/FinancialFreedom.module.css";
+import financialFreedomMobile from "../assets/Financial Freedom/Frame 1000001580.webp";
+import financialFreedomDesktop from "../assets/Financial Freedom/Frame 31 (1).webp";
+import banknotesvg from "../assets/Financial Freedom/banknotes.svg";
+import circlesvg from "../assets/Financial Freedom/circle-stack.svg";
+import arrowsvg from "../assets/Financial Freedom/arrows-right-left.svg";
 function FinancialFreedom() {
   return (
     <div className={styles.container}>
@@ -13,10 +19,39 @@ function FinancialFreedom() {
           we offer you the freedom you deserve
         </p>
       </header>
-          <main>
-              
+      <main>
+        <img src={financialFreedomMobile} alt="mobile screen" className={styles.mobileIMG} />
+        <img src={financialFreedomDesktop} alt="mobile screen" className={styles.desktopIMG} />
       </main>
-      <footer></footer>
+      <footer>
+        <section>
+          <div>
+            <ReactSVG src={banknotesvg} />
+            <h5>No minimum balance fees</h5>
+          </div>
+          <p>Say goodbye to minimum balance fees. Your account, your balance—no hidden charges</p>
+        </section>
+        <section>
+          <div>
+            <ReactSVG src={circlesvg} />
+            <h5>No monthly fees</h5>
+          </div>
+          <p>
+            Bank with us without worrying about monthly fees. Keep more of your money where it
+            belongs—in your account
+          </p>
+        </section>
+        <section>
+          <div>
+            <ReactSVG src={arrowsvg} />
+            <h5>No bank transfer fees</h5>
+          </div>
+          <p>
+            Seamlessly transfer funds without the extra cost. Send and receive money with zero bank
+            transfer fees.
+          </p>
+        </section>
+      </footer>
       <aside></aside>
     </div>
   );

9e169e164dc5cf621ab64ca287379ffef8afe187
Kwabs-97 <khobewayne@gmail.com>
2023-10-29 11:37:11 +0000

    setting up elevate finances and the various svg imports

--- src/components/ElevateFinances.js
+++ src/components/ElevateFinances.js
@@ -1,14 +1,16 @@
 /** @format */
 
-import React from "react";
-import styles from '../styles/ElevateFinances.'
-
-
+import styles from "../styles/ElevateFinances.module.css";
+import seamlessSVG from "../assets/ElevateFinances SVGs/Frame 88.svg";
+import smartInvestingSVG from "../assets/ElevateFinances SVGs/Frame 1000001576.svg";
+import wealth from "../assets/ElevateFinances SVGs/Frame 1000001577.svg";
+import financialPlanning from "../assets/ElevateFinances SVGs/Frame 1000001578.svg";
 function ElevateFinances() {
   return (
-    <>
-   
-    </>
+    <div className={styles.container}>
+      <section className={styles.section1}></section>
+      <section className={styles.section2}></section>
+    </div>
   );
 }
 

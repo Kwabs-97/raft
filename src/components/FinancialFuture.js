35049e8cea7af45233ca45f684d4a60c39295d35
Kwabs-97 <khobewayne@gmail.com>
2023-10-29 14:17:15 +0000

    financial future heading

--- src/components/FinancialFuture.js
+++ src/components/FinancialFuture.js
@@ -1,15 +1,23 @@
-import React from 'react'
-import styles from '../styles/FinancialFuture.module.css'
+/** @format */
+
+import React from "react";
+import styles from "../styles/FinancialFuture.module.css";
 
 function FinancialFuture() {
   return (
     <div className={styles.container}>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div className={styles.heading}>
+        <h4>Confidently Shape Your Financial Future</h4>
+        <p>
+          display: flex; width: 21.375rem; flex-direction: column; align-items: flex-start; gap:
+          1.5rem;
+        </p>
+      </div>
+      <div className={styles.cards}></div>
+      <div className={styles.numbers}></div>
+      <div className={styles.image}></div>
     </div>
-  )
+  );
 }
 
-export default FinancialFuture
+export default FinancialFuture;

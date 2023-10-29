619a08c7d10f59bd2514f1acd42d08c3fc2ba7c3
Kwabs-97 <khobewayne@gmail.com>
2023-10-29 11:15:47 +0000

    clearing elevate financial freedom

--- src/components/FinancialFreedom.js
+++ src/components/FinancialFreedom.js
@@ -1,17 +1,21 @@
 /** @format */
 
 import React from "react";
-import styles from '../styles/FinancialFreedom.module.css'
+import styles from "../styles/FinancialFreedom.module.css";
 
 function FinancialFreedom() {
   return (
-    <div className={styles.container} >
-          <header>
-              <h4>
-                  Your Financial Freedom, Your Way
-              </h4>
+    <div className={styles.container}>
+      <header>
+        <h4>Your Financial Freedom, Your Way</h4>
+        <p>
+          We believe that managing your finances should be effortless and cost-effective. That's why
+          we offer you the freedom you deserve
+        </p>
       </header>
-      <main></main>
+          <main>
+              
+      </main>
       <footer></footer>
       <aside></aside>
     </div>

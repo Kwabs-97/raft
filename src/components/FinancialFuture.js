5645078f7e408b8f4026a0cf24f3f294bf30a441
Kwabs-97 <khobewayne@gmail.com>
2023-10-29 14:13:03 +0000

    Financial Future Component start

---
+++ src/components/FinancialFuture.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from '../styles/FinancialFuture.module.css'
+
+function FinancialFuture() {
+  return (
+    <div className={styles.container}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
+
+export default FinancialFuture

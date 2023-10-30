98c8fe88558235d000818473a6316843b64f62fd
Kwabs-97 <khobewayne@gmail.com>
2023-10-30 13:26:20 +0000

    clearing Main

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -5,7 +5,6 @@ import HeroSection1 from "../components/HeroSection1";
 import Features from "../components/Features";
 import ElevateFinances from "../components/ElevateFinances";
 import FinancialFreedom from "../components/FinancialFreedom";
-import FinancialFuture from "../components/FinancialFuture";
 
 function Main() {
   return (
